25bb80113012064e7d42605ad2fc20d58aa08392
Cyn0213 <99071804+Cyn0213@users.noreply.github.com>
2022-12-20 20:26:31 -0800

    Positive better than negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,8 @@
 
 /**
  * main - Entry point
- * Description - Prints if randoms generated number is +tive, zero or -tive
- * Retuen: Always 0 (Success)
+ * Description - Prints if random generated number is +tive, zero or -tive
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

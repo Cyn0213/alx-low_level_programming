283d9675d8ffbf0cfea52a9c18f6a6d8fd4d5e26
Cyn0213 <99071804+Cyn0213@users.noreply.github.com>
2022-12-21 01:01:34 -0800

    3 Numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,15 +11,15 @@ int main(void)
 	int n;
 	int j;
 
-	for (i = 48; i < 58, i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (n = 48; n < 58; n++)
+		for (n =48; n < 58; n++)
 		{
 			if (n > i)
 			{
 				for (j = 48; j < 58; j++)
 				{
-					if (j>n)
+					if (j > n)
 					{
 						putchar(i);
 						putchar(n);
@@ -31,12 +31,11 @@ int main(void)
 
 						putchar(',');
 						putchar(' ');
-						}
+					}
 				}
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }

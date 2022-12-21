5417fa7b9b54d77b5bca00aaf5a0b6b5a4af6a08
Cyn0213 <99071804+Cyn0213@users.noreply.github.com>
2022-12-20 22:01:21 -0800

    Mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <ctdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c>= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
